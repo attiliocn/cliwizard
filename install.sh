cfe7b28d284b65ca7342897fba4156fc5c269e18
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-05-31 17:54:44 -0300

    fix: update install file

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Get the path of the script's directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 # Source each function file
-source "$SCRIPT_DIR/functions.sh"
+source "$CLIWIZARD_HOME/functions.sh"
