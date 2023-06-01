e4263f22a70e209e97475b5b102a38522fb9bbe7
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-06-01 10:11:24 -0300

    feat: add function to distribute k files into n folders

--- functions.sh
+++ functions.sh
@@ -31,3 +31,28 @@ function save_image_from_clipboard() {
 function sort_python_inputs() {
     xsel -b > sort.py && isort sort.py -d | xsel -b && rm sort.py
 }
+
+function distribute_files() {
+    extension="$1"  # File extension
+    num="$2"       # Number of files per folder
+
+    # Create a directory to hold the distributed files
+    mkdir -p distributed_files
+
+    # Get all files with the specified extension
+    files=($(find . -maxdepth 1 -type f -name "*."$extension"" | sort -V))
+
+    # Calculate the number of folders required
+    num_files=${#files[@]}
+    num_folders=$(((num_files + num - 1) / num))
+
+    # Create the necessary folders
+    for ((i = 1; i <= num_folders; i++)); do
+        mkdir -p "distributed_files/folder$i"
+    done
+
+    for i in {1.."$num_folders"}; do
+        find . -maxdepth 1 -type f -name "*."$extension"" | head -n "$num" | xargs -I{} mv -v {} "distributed_files/folder"$i""
+    done
+}
+
